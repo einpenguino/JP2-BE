543108d9ac70193db9436339754e8f4e304138c9
einpenguino <youyuneugene@gmail.com>
2023-02-06 08:27:55 +0800

    -

---
+++ prisma/seed_data.ts
@@ -0,0 +1,6 @@
+export const data =  [
+        { name: 'Bob', email: 'bob@prisma.io' },
+        { name: 'Bobo', email: 'bob@prisma.io' }, // Duplicate unique key!
+        { name: 'Yewande', email: 'yewande@prisma.io' },
+        { name: 'Angelique', email: 'angelique@prisma.io' },
+]
